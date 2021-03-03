a0869e4e4fda1c4ffb5a5c31570461d0bf65adf7
Andrew Robinson <andrew0robinson@yahoo.co.uk>
2021-03-03 21:21:24 +0000

    Commiting completed solution

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function calculateBill() {}
+function calculateBill(bill, vat, tipRate) {
+  if (
+    typeof bill != "number" ||
+    typeof vat != "number" ||
+    typeof tipRate != "number"
+  ) {
+    return "error";
+  } else {
+    return `£${bill + vat + tipRate}`;
+  }
+}
+
+calculateBill(100, 20, 10);
 
 module.exports = calculateBill;
